5b17ca1e5686d6fc491eeecba48c98783fce6816
jenny-tmg <jennytmg09@gmail.com>
2023-08-13 17:50:25 +0545

    initial commit

--- toDoApp/models.py
+++ toDoApp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 class ToDo(models.Model):
     task = models.CharField(max_length =100)
-    completed = models.Boolean
+    completed = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.task
